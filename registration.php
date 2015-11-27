2b25be5afceeca704c196a498f84739dcfe9dd6a
sewwwer <www.svr@gmail.com>
2015-11-27 09:51:47 +0200

    fix composer descr

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Magento_Fedex',
+    'Magento_TMOcourier',
     __DIR__
 );
