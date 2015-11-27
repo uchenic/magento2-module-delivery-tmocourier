f581e313098eb600914c9f629c6dd6f6ad7e6bfd
sewwwer <www.svr@gmail.com>
2015-11-27 09:49:27 +0200

    init

---
+++ Model/Carrier.php
@@ -0,0 +1,202 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\SampleShippingProvider\Model;
+
+use Magento\Framework\Module\Dir;
+use Magento\Quote\Model\Quote\Address\RateRequest;
+use Magento\Shipping\Model\Carrier\AbstractCarrierOnline;
+use Magento\Shipping\Model\Rate\Result;
+use Magento\Framework\Xml\Security;
+
+/**
+ * Fedex shipping implementation
+ *
+ * @author     Magento Core Team <core@magentocommerce.com>
+ * @SuppressWarnings(PHPMD.ExcessiveClassComplexity)
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class Carrier extends AbstractCarrierOnline implements \Magento\Shipping\Model\Carrier\CarrierInterface
+{
+    /**
+     * Code of the carrier
+     *
+     * @var string
+     */
+    const CODE = 'fedex';
+
+    /**
+     * Purpose of rate request
+     *
+     * @var string
+     */
+    const RATE_REQUEST_GENERAL = 'general';
+
+    /**
+     * Purpose of rate request
+     *
+     * @var string
+     */
+    const RATE_REQUEST_SMARTPOST = 'SMART_POST';
+
+    /**
+     * Code of the carrier
+     *
+     * @var string
+     */
+    protected $_code = self::CODE;
+
+    
+
+    /**
+     * Rate request data
+     *
+     * @var RateRequest|null
+     */
+    protected $_request = null;
+
+    /**
+     * Rate result data
+     *
+     * @var Result|null
+     */
+    protected $_result = null;
+
+    /**
+     * Path to wsdl file of rate service
+     *
+     * @var string
+     */
+    protected $_rateServiceWsdl;
+
+    
+
+    
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $_storeManager;
+
+    /**
+     * @var \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory
+     */
+    protected $_productCollectionFactory;
+
+    /**
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory $rateErrorFactory
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param Security $xmlSecurity
+     * @param \Magento\Shipping\Model\Simplexml\ElementFactory $xmlElFactory
+     * @param \Magento\Shipping\Model\Rate\ResultFactory $rateFactory
+     * @param \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $rateMethodFactory
+     * @param \Magento\Shipping\Model\Tracking\ResultFactory $trackFactory
+     * @param \Magento\Shipping\Model\Tracking\Result\ErrorFactory $trackErrorFactory
+     * @param \Magento\Shipping\Model\Tracking\Result\StatusFactory $trackStatusFactory
+     * @param \Magento\Directory\Model\RegionFactory $regionFactory
+     * @param \Magento\Directory\Model\CountryFactory $countryFactory
+     * @param \Magento\Directory\Model\CurrencyFactory $currencyFactory
+     * @param \Magento\Directory\Helper\Data $directoryData
+     * @param \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistry
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\Module\Dir\Reader $configReader
+     * @param \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
+     * @param array $data
+     *
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory $rateErrorFactory,
+        \Psr\Log\LoggerInterface $logger,
+        Security $xmlSecurity,
+        \Magento\Shipping\Model\Simplexml\ElementFactory $xmlElFactory,
+        \Magento\Shipping\Model\Rate\ResultFactory $rateFactory,
+        \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $rateMethodFactory,
+        \Magento\Shipping\Model\Tracking\ResultFactory $trackFactory,
+        \Magento\Shipping\Model\Tracking\Result\ErrorFactory $trackErrorFactory,
+        \Magento\Shipping\Model\Tracking\Result\StatusFactory $trackStatusFactory,
+        \Magento\Directory\Model\RegionFactory $regionFactory,
+        \Magento\Directory\Model\CountryFactory $countryFactory,
+        \Magento\Directory\Model\CurrencyFactory $currencyFactory,
+        \Magento\Directory\Helper\Data $directoryData,
+        \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistry,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\Module\Dir\Reader $configReader,
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
+        array $data = []
+    ) {
+        $this->_storeManager = $storeManager;
+        $this->_productCollectionFactory = $productCollectionFactory;
+        parent::__construct(
+            $scopeConfig,
+            $rateErrorFactory,
+            $logger,
+            $xmlSecurity,
+            $xmlElFactory,
+            $rateFactory,
+            $rateMethodFactory,
+            $trackFactory,
+            $trackErrorFactory,
+            $trackStatusFactory,
+            $regionFactory,
+            $countryFactory,
+            $currencyFactory,
+            $directoryData,
+            $stockRegistry,
+            $data
+        );
+       
+        
+    }
+
+    /**
+     * Get allowed shipping methods
+     *
+     * @return array
+     */
+    public function getAllowedMethods()
+    {
+        return [$this->getCarrierCode() => __($this->getConfigData('name'))];
+    }
+
+  
+    protected function _doShipmentRequest(\Magento\Framework\DataObject $request){
+        return 0;
+    };
+   
+
+    
+
+    
+
+    /**
+     * Get configured Store Shipping Origin
+     *
+     * @return array
+     */
+    protected function getShippingOrigin()
+    {
+        return [
+            'country_id' => $this->_scopeConfig->getValue(
+                Config::XML_PATH_ORIGIN_COUNTRY_ID,
+                ScopeInterface::SCOPE_STORE,
+                $this->getStore()),
+            'region_id' => $this->_scopeConfig->getValue(
+                Config::XML_PATH_ORIGIN_REGION_ID,
+                ScopeInterface::SCOPE_STORE,
+                $this->getStore()),
+            'postcode' => $this->_scopeConfig->getValue(
+                Config::XML_PATH_ORIGIN_POSTCODE,
+                ScopeInterface::SCOPE_STORE,
+                $this->getStore()),
+            'city' => $this->_scopeConfig->getValue(
+                Config::XML_PATH_ORIGIN_CITY,
+                ScopeInterface::SCOPE_STORE,
+                $this->getStore())
+        ];
+    }
+}
